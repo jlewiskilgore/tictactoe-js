21ccb9166a2b13b966febe2dba92001a4df90b4e
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-19 23:03:57 -0500

    Mark spaces with alternative player symbols

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,7 +1,35 @@
 $( document ).ready(function() {
-    var player1Symbol = "X";
-    var player2Symbol = "O";
+	startGame();
+})
 
-    var currentPlayer = 1; //Player 1 starts the game
-    var isGameOver = 0;
-});
+function startGame() {
+	player1Symbol = "X";
+    player2Symbol = "O";
+
+    currentPlayer = 1; //Player 1 starts the game
+    isGameOver = 0;
+}
+
+function checkSpace(space) {
+	console.log("test");
+	console.log(space);
+	if(currentPlayer == 1) {
+		markPlayerSpace(currentPlayer, space);
+	}
+	else if(currentPlayer == 2) {
+		markPlayerSpace(currentPlayer, space);
+	}
+}
+
+//Return true if a valid move was made
+function markPlayerSpace(player, space) {
+	var spaceButton = document.getElementById(space);
+	if(spaceButton.value == " " && player == 1) {
+		spaceButton.value = player1Symbol;
+		currentPlayer = 2;
+	}
+	else if(spaceButton.value == " " && player == 2) {
+		spaceButton.value = player2Symbol;
+		currentPlayer = 1;
+	}
+}
